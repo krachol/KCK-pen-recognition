c9d133ce9d70adfa41cbc1a204d224622b0e981c
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-11-26 21:43:06 +0100

    Extract template matching to seperate module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+import template_matching
 
 if __name__ == '__main__':
     # Get camera
@@ -7,40 +7,16 @@ if __name__ == '__main__':
 
     # Read in template image and save its dimensions
     template = cv2.imread('templete.png', 0)
-    w, h = template.shape[::-1]
 
     # List wanted methods
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-               'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
     while (1):
-
         _, img = cap.read()  # Read frame,
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change it to greyscale,
         img = cv2.flip(img, 1)  # flip along y axis
-        img2 = img.copy()  # and copy to have original stored
 
         # Test all methods
-        for meth in methods:
-            method = eval(meth)
-
-            img = img2.copy() # Get original frame
-
-            # Draw rectangle on matches
-            # Apply template Matching
-            res = cv2.matchTemplate(img, template, method)
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-            # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-            if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-                top_left = min_loc
-            else:
-                top_left = max_loc
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-
-            cv2.rectangle(img, top_left, bottom_right, 255, 2)
-
-            # Show frame
-            cv2.imshow(meth, img)
+        for name, matched in template_matching.get_next_template_match(img, template):
+            cv2.imshow(name, matched)
 
         # Wait on Esc to close
         k = cv2.waitKey(5) & 0xFF
---
+++ template_matching.py
@@ -0,0 +1,31 @@
+import cv2
+
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+           'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+
+def get_next_template_match(in_img, template, original=None):
+    w, h = template.shape[::-1]
+    if original == None:
+        original = in_img
+
+    for meth in methods:
+        method = eval(meth)
+
+        img = in_img.copy()  # Get original frame
+
+        # Draw rectangle on matches
+        # Apply template Matching
+        res = cv2.matchTemplate(img, template, method)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+
+        cv2.rectangle(original, top_left, bottom_right, 255, 2)
+
+        yield meth, original
