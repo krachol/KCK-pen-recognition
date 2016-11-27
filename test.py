12d5e2d2010d136f9f82e47e466c2f76454f8dac
Tomasz Kasperek <kasperekt@gmail.com>
2016-11-27 12:06:55 +0100

    Shit

--- test.py
+++ test.py
@@ -10,29 +10,31 @@ if __name__ == '__main__':
 
     # List wanted methods
     while (1):
+        _, frame = cap.read()  # Read frame,
+        original_frame = frame.copy()
 
-        _, img = cap.read()  # Read frame,
+        flipped = cv2.flip(frame, 1)
+        blurred = cv2.GaussianBlur(flipped, (11, 11), 0)
+
+        mask = frame
+        mask = fgbg.apply(blurred)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        # mask = cv2.adaptiveThreshold(mask, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 0)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
         
-        img = fgbg.apply(img)
-        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change it to greyscale,
-        # img = cv2.flip(img, 1)  # flip along y axis
-        # img = cv2.medianBlur(img, 5)
-
-        # canny = cv2.Canny(img, 100, 200)
-        # blur = cv2.GaussianBlur(img,(5,5),0)
-        # thres1 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        # _, thres2 = cv2.threshold(blur,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
+        canny = cv2.Canny(mask, 100, 200)
+        
         # (_, cnts, _) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # cv2.imshow('Test', canny)
-
 
         # cv2.imshow('Threshold', thres2)
         # for c in cnts:
         #     cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
 
-        cv2.imshow('test', img)
+        cv2.imshow('Frame', original_frame)
+        cv2.imshow('Mask', mask)
+        cv2.imshow('Canny', canny)
 
         # Wait on Esc to close
         k = cv2.waitKey(5) & 0xFF
